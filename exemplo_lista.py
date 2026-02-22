e6a5c93233cde0cb00b1783d96b57e5df63cf3cd
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-02-22 20:20:14 -0300

    Segundo Commit

--- exemplo_lista.py
+++ exemplo_lista.py
@@ -1,12 +1,40 @@
-produto_1: str = "Sapato"
-produto_2: str = "Camiseta"
-produto_3: str = "Videogame"
 
-produtos: list = []
+# produto_1: str = "Sapato"
+# produto_2: str = "Camiseta"
+# produto_3: str = "Videogame"
 
-produtos.append(produto_1)
-produtos.append(produto_2)
-produtos.append(produto_3)
+# produtos: list = []
+
+# produtos.append(produto_1)
+# produtos.append(produto_2)
+# produtos.append(produto_3)
 
 
-print(produtos)
+# print(produtos)
+
+# lista = ["Sapato", 39, 10.38, True]
+
+# print(lista)
+
+produto_0001 = {
+
+    "nome": "Sapato",
+    "quantidade": 39,
+    "preco": 10.38,
+    "disponibilidade": True
+},
+
+produto_0002 = {
+    "Nome": "Televisão",
+    "quantidade": 10,
+    "preco": 70.38,
+    "disponibilidade": False
+}
+
+
+carrinho_compras: list = []
+
+carrinho_compras.append(produto_0001)
+carrinho_compras.append(produto_0002)
+
+print(carrinho_compras)
