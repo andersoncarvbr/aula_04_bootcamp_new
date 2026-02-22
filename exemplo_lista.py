7645eada67d4bc7cdfdffda260f674fca4eda7df
Anderson Brito de Carvalho <andersoncarv@hotmail.com>
2026-02-22 19:59:15 -0300

    Primeiro Commit

---
+++ exemplo_lista.py
@@ -0,0 +1,12 @@
+produto_1: str = "Sapato"
+produto_2: str = "Camiseta"
+produto_3: str = "Videogame"
+
+produtos: list = []
+
+produtos.append(produto_1)
+produtos.append(produto_2)
+produtos.append(produto_3)
+
+
+print(produtos)
